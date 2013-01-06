7646c88c70e41b824277d45ff4f31e8c115112c2
Nick <nick@astonishdesigns.com>
2013-01-06 13:04:47 -0600

    added webfonts, background images for boxes, etc.

--- all/modules/custom/foodisfree_blocks/block-donate.tpl.php
+++ all/modules/custom/foodisfree_blocks/block-donate.tpl.php
@@ -1,1 +1,2 @@
 <h1>Donate!</h1>
+<p>help us out by donating</p>
--- all/themes/FoodisFree/page.tpl.php
+++ all/themes/FoodisFree/page.tpl.php
@@ -7,50 +7,34 @@
    <?php endif; ?>
 
     <?php print render($page['header']); ?>
-
-    <?php if (!$front_page): ?>
-      <?php if ($main_menu || $secondary_menu || !empty($page['navigation'])): ?>
-        <nav id="navigation" role="navigation" class="clearfix">
-          <?php if (!empty($page['navigation'])): ?> <!--if block in navigation region, override $main_menu and $secondary_menu-->
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-          <?php if (empty($page['navigation'])): ?>
-  		  <?php print theme('links__system_main_menu', array(
-              'links' => $main_menu,
-              'attributes' => array(
-                'id' => 'main-menu',
-                'class' => array('links', 'clearfix'),
-              ),
-              'heading' => array(
-                'text' => t('Main menu'),
-                'level' => 'h2',
-                'class' => array('element-invisible'),
-              ),
-            )); ?>
-          <?php endif; ?>
-        </nav> <!-- /#navigation -->
-      <?php endif; ?>
-      <?php if ($breadcrumb): print $breadcrumb; endif;?>
-    <?php endif; ?>
+    <?php if ($breadcrumb): print $breadcrumb; endif;?>
   </header> <!-- /#header -->
 
   <section id="main" role="main" class="clearfix">
     <?php print $messages; ?>
+
     <?php print render($title_prefix); ?>
     <?php if (!$front_page): ?>
       <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
-    <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+
+    <?php if (!empty($tabs['#primary'])): ?>
+      <div class="tabs-wrapper clearfix">
+        <?php print render($tabs); ?>
+      </div>
+    <?php endif; ?>
+
     <?php print render($page['help']); ?>
-    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 
     <?php if ($page['pre_content']): ?>
       <section id="pre-content">
         <?php print render($page['pre_content']); ?>
       </section>
     <?php endif; ?>
+
     <?php print render($page['content']); ?>
+
     <?php if ($page['post_content']): ?>
       <section id="post-content">
         <?php print render($page['post_content']); ?>
