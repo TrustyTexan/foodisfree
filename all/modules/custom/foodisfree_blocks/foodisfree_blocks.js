87177f6c9acc4d542902cfd3ccfb8e066d41db03
David Salgado <david.b.salgado@gmail.com>
2013-01-12 19:42:31 -0600

    Finished front-end logic for donate block.

---
+++ all/modules/custom/foodisfree_blocks/foodisfree_blocks.js
@@ -0,0 +1,25 @@
+(function($) {
+    $(document).ready(function(){
+        var form  = $("#donateBlockForm")[0],
+            nextButton = $("#donateBlockForm .nextButton")[0],
+            amountInput = $("#donateBlockForm :input[name='amount']")[0],
+            submitted = false,
+            validateFormat = function(inputVal){
+                if (submitted) return false;
+                if (!inputVal || isNaN(inputVal) || inputVal < 0){
+                    alert("Please enter a valid dollar amount. Thank you!");
+                    return false;
+                }
+                amountInput.value = parseFloat(inputVal, 10).toFixed(2);
+                submitted = true;
+                return true;
+            };
+
+        form.onsubmit = function(){
+            return validateFormat(amountInput.value);
+        };
+        nextButton.onclick = function(e) {
+            if (validateFormat(amountInput.value)) form.submit();
+        };
+    });
+})(jQuery);
