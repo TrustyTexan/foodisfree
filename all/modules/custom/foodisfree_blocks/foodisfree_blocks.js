de7673287ad36da688ca7d4d3e71e4acc4a0db67
David Salgado <david.b.salgado@gmail.com>
2013-01-19 21:56:02 -0600

    Added auto focus to donate dollar input on body load.

--- all/modules/custom/foodisfree_blocks/foodisfree_blocks.js
+++ all/modules/custom/foodisfree_blocks/foodisfree_blocks.js
@@ -4,12 +4,15 @@
             nextButton = $("#donateBlockForm .nextButton")[0],
             amountInput = $("#donateBlockForm :input[name='amount']")[0],
             submitted = false,
+
             validateFormat = function(inputVal){
                 if (submitted) return false;
+
                 if (!inputVal || isNaN(inputVal) || inputVal < 0){
                     alert("Please enter a valid dollar amount. Thank you!");
                     return false;
                 }
+
                 amountInput.value = parseFloat(inputVal, 10).toFixed(2);
                 submitted = true;
                 return true;
@@ -21,5 +24,7 @@
         nextButton.onclick = function(e) {
             if (validateFormat(amountInput.value)) form.submit();
         };
+
+        amountInput.focus();
     });
 })(jQuery);
